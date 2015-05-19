4b7cc6a51a1aeecc40b9bfad5809757ffe175713
Paul Marston <paulmarsy@users.noreply.github.com>
2015-05-19 09:22:33 +0100

    Adding try/catch logic to Write-VcsStatus
    Adding try/catch logic to Write-VcsStatus so that if one of the scriptblocks that has been added to the VcsPromptStatuses array fails the other scriptblocks are not affected (are still invoked)
    
    This complements the posh-git change https://github.com/dahlbyk/posh-git/pull/170

--- SvnPrompt.ps1
+++ SvnPrompt.ps1
@@ -7,6 +7,9 @@ $global:SvnPromptSettings = New-Object PSObject -Property @{
     AfterForegroundColor      = [ConsoleColor]::Yellow
     AfterBackgroundColor      = $Host.UI.RawUI.BackgroundColor
     
+    ErrorForegroundColor      = [ConsoleColor]::Red
+    ErrorBackgroundColor      = $Host.UI.RawUI.BackgroundColor
+    
     BranchForegroundColor    = [ConsoleColor]::Cyan
     BranchBackgroundColor    = $Host.UI.RawUI.BackgroundColor
     
@@ -48,10 +51,29 @@ function Write-SvnStatus($status) {
 }
 
 # Should match https://github.com/dahlbyk/posh-git/blob/master/GitPrompt.ps1
-if((Get-Variable -Scope Global -Name VcsPromptStatuses -ErrorAction SilentlyContinue) -eq $null) {
-    $Global:VcsPromptStatuses = @()
+if (!$Global:VcsPromptStatuses) { $Global:VcsPromptStatuses = @() }
+
+function Global:Write-VcsStatus {
+    $Global:VcsPromptStatuses | foreach {
+        $vcsPromptErrors = $null
+        try {
+            Invoke-Command -ScriptBlock $_ -ErrorVariable vcsPromptErrors 2>$null
+        }
+        catch {
+            $vcsPromptErrors = $_
+        }
+        if ($vcsPromptErrors.Length -gt 0) {
+             # Log the errors but dont affect the users prompt, splat error objects into Write-Error
+            $vcsPromptErrors | % { Write-Error -ErrorAction SilentlyContinue @_ }
+            $s = $Global:SvnPromptSettings
+            if ($s) {
+                Write-Prompt $s.BeforeText -BackgroundColor $s.BeforeBackgroundColor -ForegroundColor $s.BeforeForegroundColor
+                Write-Prompt "Error" -BackgroundColor $s.ErrorBackgroundColor -ForegroundColor $s.ErrorForegroundColor
+                Write-Prompt $s.AfterText -BackgroundColor $s.AfterBackgroundColor -ForegroundColor $s.AfterForegroundColor
+            }
+        }
+    }
 }
-function Global:Write-VcsStatus { $Global:VcsPromptStatuses | foreach { & $_ } }
 
 # Add scriptblock that will execute for Write-VcsStatus
 $Global:VcsPromptStatuses += {
